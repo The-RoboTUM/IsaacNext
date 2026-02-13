18740ec54378e58bcace6f1c5519675b784d4537
Linus Schwarz <public.linus.schwarz@gmail.com>
2026-02-13 11:16:38 +0100

    Action term with configclass for IsaacLab

---
+++ source/isaaclab/isaaclab/tendons/action_term_cfg.py
@@ -0,0 +1,31 @@
+"""Config class for the tendon action term."""
+
+from isaaclab.envs.manager_based_env import ManagerBasedEnv
+from isaaclab.managers.action_manager import ActionTerm
+from isaaclab.managers.manager_term_cfg import ActionTermCfg
+from isaaclab.tendons.action_term import TendonActionTerm
+from isaaclab.tendons.constants import TendonConstantRandomizationRanges
+from isaaclab.utils import configclass
+
+
+@configclass
+class TendonActionTermCfg(ActionTermCfg):
+    """Configuration for tendon-based action term."""
+
+    class_type: type[ActionTerm] = TendonActionTerm
+    """The associated action term class.
+
+    The class should inherit from :class:`isaaclab.managers.action_manager.ActionTerm`.
+    """
+
+    asset_name: str = "robot"
+    """The name of the scene entity.
+
+    This is the name defined in the scene configuration file. See the :class:`InteractiveSceneCfg`
+    class for more details.
+    """
+
+    randomization_ranges: TendonConstantRandomizationRanges = (
+        TendonConstantRandomizationRanges()
+    )
+    """Randomization ranges for tendon constants."""
